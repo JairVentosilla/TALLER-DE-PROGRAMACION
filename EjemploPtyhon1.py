7be387d051c691d59a166f55b13492dba30ba78b
JairVentosilla <50757332+JairVentosilla@users.noreply.github.com>
2021-11-16 23:41:38 -0500

    Add files via upload

---
+++ EjemploPtyhon1.py
@@ -0,0 +1,8 @@
+for i in range(12,0,-2):
+    for j in range(0,i,1):
+        print("*",end="")
+    print("")
+for i in range(2,12,2):
+    for j in range(0,i+2,1):
+        print("*",end="")
+    print("")
